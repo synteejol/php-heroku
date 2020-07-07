c68bda41b2a205a518eee91b8c9effbb270f3cd6
synteejol <carmelo.marasco1787@gmail.com>
2020-07-07 15:42:55 +0200

    json data

--- web/test.php
+++ web/test.php
@@ -3,7 +3,23 @@
 <body>
 
 <?php
-echo "ciao ahjhaksjaj";
+$myArray = array();
+$myArray[0] -> id = "1"
+$myArray[0] -> nome = "sezione1"
+$myArray[0] -> descrizione = "breve descrizione"
+$myArray[0] -> immagine = "https://images.unsplash.com/photo-1507591064344-4c6ce005b128?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=500&h=350&fit=crop&ixid=eyJhcHBfaWQiOjF9"
+
+$myArray[1] -> id = "1"
+$myArray[1] -> nome = "sezione1"
+$myArray[1] -> descrizione = "breve descrizione"
+$myArray[1] -> immagine = "https://images.unsplash.com/photo-1507591064344-4c6ce005b128?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=500&h=350&fit=crop&ixid=eyJhcHBfaWQiOjF9"
+
+$myArray[2] -> id = "1"
+$myArray[2] -> nome = "sezione1"
+$myArray[2] -> descrizione = "breve descrizione"
+$myArray[2] -> immagine = "https://images.unsplash.com/photo-1507591064344-4c6ce005b128?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=500&h=350&fit=crop&ixid=eyJhcHBfaWQiOjF9"
+
+print json_encode($myArray);
 ?>
 
 </body>
