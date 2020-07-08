fd5dc3834a8153a6341dc268a6f64f05cafb22e3
synteejol <carmelo.marasco1787@gmail.com>
2020-07-08 15:07:22 +0200

    prova imgur

--- web/db_conn.php
+++ web/db_conn.php
@@ -20,7 +20,7 @@ $myArray[1] ['immagine'] = "https://images.unsplash.com/photo-1507591064344-4c6c
 $myArray[2] ['id'] = 3;
 $myArray[2] ['nome'] = "sezione3";
 $myArray[2] ['descrizione'] = "breve descrizione";
-$myArray[2] ['immagine'] = "https://images.unsplash.com/photo-1507591064344-4c6ce005b128?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=500&h=350&fit=crop&ixid=eyJhcHBfaWQiOjF9";
+$myArray[2] ['immagine'] = "https://imgur.com/gallery/9SdN4nM";
 
 
 print json_encode($myArray);
