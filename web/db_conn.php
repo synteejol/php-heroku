0c871d40208197b444b2797297849ffdd69a5556
synteejol <carmelo.marasco1787@gmail.com>
2020-07-08 14:58:40 +0200

    controllo accesso sezioni

--- web/db_conn.php
+++ web/db_conn.php
@@ -23,4 +23,20 @@ $myArray[2] ['descrizione'] = "breve descrizione";
 $myArray[2] ['immagine'] = "https://images.unsplash.com/photo-1507591064344-4c6ce005b128?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=500&h=350&fit=crop&ixid=eyJhcHBfaWQiOjF9";
 
 
-print json_encode($myArray);
+if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
+    $link = "https";
+else
+    $link = "http";
+
+// Here append the common URL characters.
+$link .= "://";
+
+// Append the host(domain name, ip) to the URL.
+$link .= $_SERVER['HTTP_HOST'];
+
+// Append the requested resource location to the URL
+$link .= $_SERVER['REQUEST_URI'];
+
+// Print the link
+if ($link === 'https://testquitv.herokuapp.com/')
+    print json_encode($myArray);
