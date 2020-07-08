6421577cc922bfa9785eec6d1714583aab4fdd4f
synteejol <carmelo.marasco1787@gmail.com>
2020-07-08 16:31:15 +0200

    imgur2

--- web/db_conn.php
+++ web/db_conn.php
@@ -10,32 +10,32 @@ $myArray = array();
 $myArray[0] ['id'] = 1;
 $myArray[0] ['nome'] = "consigli comunali";
 $myArray[0] ['descrizione'] = "breve descrizione";
-$myArray[0] ['immagine'] = "https://i.imgur.com/oTUXBio.jpg";
+$myArray[0] ['immagine'] = "https://i.imgur.com/xLl8lIm.jpg";
 
 $myArray[1] ['id'] = 2;
 $myArray[1] ['nome'] = "aggiornamenti";
 $myArray[1] ['descrizione'] = "breve descrizione";
-$myArray[1] ['immagine'] = "https://i.imgur.com/l2CvYzN.jpg";
+$myArray[1] ['immagine'] = "https://i.imgur.com/TE3JKWs.jpg";
 
 $myArray[2] ['id'] = 3;
 $myArray[2] ['nome'] = "sezione3";
 $myArray[2] ['descrizione'] = "breve descrizione";
-$myArray[2] ['immagine'] = "https://imgur.com/gallery/9SdN4nM";
+$myArray[2] ['immagine'] = "https://imgur.com/NDKptI1";
 
 
 $myArray[3] ['id'] = 4;
 $myArray[3] ['nome'] = "sezione3";
 $myArray[3] ['descrizione'] = "breve descrizione";
-$myArray[3] ['immagine'] = "https://i.imgur.com/9UGkG51.jpg";
+$myArray[3] ['immagine'] = "https://imgur.com/pGfEHKb";
 
-$myArray[4] ['id'] = 5;
-$myArray[4] ['nome'] = "sezione3";
-$myArray[4] ['descrizione'] = "breve descrizione";
-$myArray[4] ['immagine'] = "https://i.imgur.com/nSzKdjz.jpg";
-
-$myArray[5] ['id'] = 6;
-$myArray[5] ['nome'] = "sezione3";
-$myArray[5] ['descrizione'] = "breve descrizione";
-$myArray[5] ['immagine'] = "https://i.imgur.com/Nkuhgzl.jpg";
+// $myArray[4] ['id'] = 5;
+// $myArray[4] ['nome'] = "sezione3";
+// $myArray[4] ['descrizione'] = "breve descrizione";
+// $myArray[4] ['immagine'] = "https://i.imgur.com/nSzKdjz.jpg";
+//
+// $myArray[5] ['id'] = 6;
+// $myArray[5] ['nome'] = "sezione3";
+// $myArray[5] ['descrizione'] = "breve descrizione";
+// $myArray[5] ['immagine'] = "https://i.imgur.com/Nkuhgzl.jpg";
 
 print json_encode($myArray);
