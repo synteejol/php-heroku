d2e2188ef2380a5a23bb7e751134a00a29c7116c
synteejol <carmelo.marasco1787@gmail.com>
2020-07-08 16:19:34 +0200

    imgur

--- web/db_conn.php
+++ web/db_conn.php
@@ -8,19 +8,34 @@ header("Access-Control-Allow-Methods: PUT,PATCH, POST, GET, OPTIONS, DELETE");
 
 $myArray = array();
 $myArray[0] ['id'] = 1;
-$myArray[0] ['nome'] = "sezione1";
+$myArray[0] ['nome'] = "consigli comunali";
 $myArray[0] ['descrizione'] = "breve descrizione";
-$myArray[0] ['immagine'] = "https://images.unsplash.com/photo-1507591064344-4c6ce005b128?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=500&h=350&fit=crop&ixid=eyJhcHBfaWQiOjF9";
+$myArray[0] ['immagine'] = "https://i.imgur.com/oTUXBio.jpg";
 
 $myArray[1] ['id'] = 2;
-$myArray[1] ['nome'] = "sezione2";
+$myArray[1] ['nome'] = "aggiornamenti";
 $myArray[1] ['descrizione'] = "breve descrizione";
-$myArray[1] ['immagine'] = "https://images.unsplash.com/photo-1507591064344-4c6ce005b128?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=500&h=350&fit=crop&ixid=eyJhcHBfaWQiOjF9";
+$myArray[1] ['immagine'] = "https://i.imgur.com/l2CvYzN.jpg";
 
 $myArray[2] ['id'] = 3;
 $myArray[2] ['nome'] = "sezione3";
 $myArray[2] ['descrizione'] = "breve descrizione";
 $myArray[2] ['immagine'] = "https://imgur.com/gallery/9SdN4nM";
 
+
+$myArray[3] ['id'] = 4;
+$myArray[3] ['nome'] = "sezione3";
+$myArray[3] ['descrizione'] = "breve descrizione";
+$myArray[3] ['immagine'] = "https://i.imgur.com/9UGkG51.jpg";
+
+$myArray[4] ['id'] = 5;
+$myArray[4] ['nome'] = "sezione3";
+$myArray[4] ['descrizione'] = "breve descrizione";
+$myArray[4] ['immagine'] = "https://i.imgur.com/nSzKdjz.jpg";
+
+$myArray[5] ['id'] = 6;
+$myArray[5] ['nome'] = "sezione3";
+$myArray[5] ['descrizione'] = "breve descrizione";
+$myArray[5] ['immagine'] = "https://i.imgur.com/Nkuhgzl.jpg";
 
 print json_encode($myArray);
